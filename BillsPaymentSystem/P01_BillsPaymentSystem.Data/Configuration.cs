dd56061bf0f6d990fd389f872d3b5a4bcd2b25fe
Plamen Rusanov <plamen.rusanov@abv.bg>
2018-09-17 15:36:18 +0300

    Update Configuration

--- BillsPaymentSystem/P01_BillsPaymentSystem.Data/Configuration.cs
+++ BillsPaymentSystem/P01_BillsPaymentSystem.Data/Configuration.cs
@@ -7,7 +7,7 @@ namespace P01_BillsPaymentSystem.Data
     public class Configuration
     {
         public const string ConectionString = 
-            @"Server=DESKTOP-D1G8VKM\SQLEXPRESS;Database=PaymentSystem;" +
+            @"Server=.;Database=PaymentSystem;" +
             "Integrated Security=True";
     }
 }
